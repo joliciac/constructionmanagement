396d27e8777bb2817b1a38549024090e5bdaef71
joliciac <joliciacadogan07@gmail.com>
2025-03-10 16:32:18 +0000

    implemented_a_signup_button_with_functionality

--- app/src/main/java/com/example/constructionmanagement/composables/screens/SignupScreen.kt
+++ app/src/main/java/com/example/constructionmanagement/composables/screens/SignupScreen.kt
@@ -3,7 +3,9 @@ package com.example.constructionmanagement.composables.screens
 import android.widget.CheckedTextView
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -13,18 +15,25 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.constructionmanagement.viewmodel.SignupViewModel
 
 @Composable
 fun SignupScreen(onSignupSuccess: () -> Unit, onNavigateToLogin: () -> Unit) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
+    var isLoading by remember { mutableStateOf(false) }
     val roles = listOf("Worker", "Admin")
     var selectedRole by remember { mutableStateOf(roles[0]) }
+    val signupViewModel: SignupViewModel = viewModel()
+    val context = LocalContext.current
 
     Scaffold() { paddingValues ->
         Column(
@@ -46,7 +55,7 @@ fun SignupScreen(onSignupSuccess: () -> Unit, onNavigateToLogin: () -> Unit) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(0.8f)
+                    .fillMaxHeight(0.9f)
                     .padding(25.dp),
                 verticalArrangement = Arrangement.Bottom) {
                 OutlinedTextField(
@@ -90,6 +99,19 @@ fun SignupScreen(onSignupSuccess: () -> Unit, onNavigateToLogin: () -> Unit) {
                         }
                     }
                 }
+                Spacer(modifier = Modifier.height(15.dp))
+                Button(onClick = {
+                    isLoading = true
+                    signupViewModel.signup(email, password, confirmPassword, selectedRole, context) { isSuccess ->
+                        isLoading = false
+                        if (isSuccess) {
+                            onSignupSuccess()
+                        }
+                    }
+                }) {
+                    if (isLoading) CircularProgressIndicator(color = Color.White) else Text("Signup")
+                }
+
                 TextButton(onClick = onNavigateToLogin) {
                     Text("Already have an account? Login",
                         modifier = Modifier.padding(15.dp))
