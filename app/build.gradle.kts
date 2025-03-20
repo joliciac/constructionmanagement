c5317d10f64582feae82ca1b2e64347d2823d4bc
joliciac <joliciacadogan07@gmail.com>
2025-03-20 14:20:21 +0000

    added_fcm_dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,7 @@ dependencies {
     //Room Database
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.firebase.messaging)
     ksp(libs.androidx.room.compiler)
 
 
