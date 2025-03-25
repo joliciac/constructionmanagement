02ffd961994097ac5169a497921a29f8606a7917
joliciac <joliciacadogan07@gmail.com>
2025-03-25 16:08:24 +0000

    added_shape_and_implemented_to_login_screen

--- app/src/main/java/com/example/constructionmanagement/composables/screens/LoginScreen.kt
+++ app/src/main/java/com/example/constructionmanagement/composables/screens/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.constructionmanagement.composables.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.*
@@ -7,12 +8,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.constructionmanagement.R
 import com.example.constructionmanagement.viewmodel.LoginViewModel
 
 @Composable
@@ -24,65 +27,82 @@ fun LoginScreen(onLoginSuccess: (String) -> Unit, onNavigateToSignup: () -> Unit
     val context = LocalContext.current
 
     Scaffold() { paddingValues ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
-                .padding(top = 90.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(paddingValues),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.43f),
+                painter = painterResource(id = R.drawable.shape),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds
+            )
+        }
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 "Login",
                 fontSize = 38.sp,
             )
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxHeight(0.8f)
-                .padding(25.dp),
-            verticalArrangement = Arrangement.Bottom,
-        ) {
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("Email") },
-                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
+            Card(
                 modifier = Modifier
-                    .padding(bottom = 15.dp)
-                    .fillMaxWidth()
-            )
-            OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                label = { Text("Password") },
-                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
+                    .fillMaxHeight(0.6f)
+                    .padding(25.dp),
+                colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surface)
+            ) {
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Email") },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
+                    modifier = Modifier
+                        .padding(bottom = 15.dp)
+                        .fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Password") },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
 
-            Button(onClick = {
-                isLoading = true
-                loginViewModel.login(email, password, context) { isSuccess, role ->
-                    isLoading = false
-                    if (isSuccess && role != null) {
-                        onLoginSuccess(role)
+                Button(onClick = {
+                    isLoading = true
+                    loginViewModel.login(email, password, context) { isSuccess, role ->
+                        isLoading = false
+                        if (isSuccess && role != null) {
+                            onLoginSuccess(role)
+                        }
                     }
                 }
-            }) {
-                if (isLoading) CircularProgressIndicator(color = MaterialTheme.colorScheme.surface) else Text("Login")
-            }
-            Spacer(modifier = Modifier.height(16.dp))
+                ) {
+                    if (isLoading) CircularProgressIndicator(color = MaterialTheme.colorScheme.surface) else Text(
+                        "Login"
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
 
-            TextButton(onClick = { loginViewModel.showForgotPasswordDialog() }) {
-                Text("Forgot Password?")
+                TextButton(onClick = { loginViewModel.showForgotPasswordDialog() }) {
+                    Text("Forgot Password?")
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                TextButton(onClick = onNavigateToSignup) {
+                    Text("Don't have an account? Sign up")
+                }
             }
-            Spacer(modifier = Modifier.height(8.dp))
-            TextButton(onClick = onNavigateToSignup) {
-                Text("Don't have an account? Sign up")
-            }
-        }
     }
 }
+
 
 @Preview
 @Composable
